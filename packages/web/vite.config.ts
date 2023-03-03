7b5264f989ac830ed23537492c324c67048cad89
yiwwhl <yiwwhl@qq.com>
2023-03-03 23:41:20 +0800

    feat: basic vite config

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
+import { defineConfig, UserConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+enum MODE {
+  DEVELOPMENT = 'development',
+  PRODUCTION = 'production',
+}
+
+const modeConfigMap: Record<MODE, UserConfig> = {
+  [MODE.DEVELOPMENT]: {
+    server: {
+      host: '127.0.0.1',
+      port: 6006,
+      proxy: {
+        '/dangdang': {
+          target: 'http://127.0.0.1:6008',
+        },
+      },
+    },
+  },
+  [MODE.PRODUCTION]: {},
+}
+
+export default defineConfig((configEnv) => {
+  const mode = configEnv.mode
+  return {
+    plugins: [vue()],
+    ...modeConfigMap[mode],
+  }
 })
