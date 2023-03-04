0ef749cfe8ccd31990a0cca4ea96f171c0083772
yiwwhl <yiwwhl@qq.com>
2023-03-04 22:06:18 +0800

    feat: shared yw-storage

---
+++ packages/shared/index.ts
@@ -0,0 +1,1 @@
+export * from './utils/yw-storage/index'
--- packages/web/src/main.ts
+++ packages/web/src/main.ts
@@ -3,11 +3,4 @@ import App from './App'
 
 const app = createApp(App)
 
-const obj = {
-  name: '1',
-  age: 2,
-}
-
-const myKey = Object.keys(obj)
-
 app.mount('#app')
