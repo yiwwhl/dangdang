1fd874191b62ebf10ea43e1cad05a36fa8df2fc2
yiwwhl <yiwwhl@yiwwhl.local>
2023-03-09 17:53:57 +0800

    chore: typease

--- packages/server/src/server.d.ts
+++ packages/server/src/server.d.ts
@@ -1,6 +1,6 @@
 /// <reference types="shared/declare/expansion" />
 /// <reference types="shared/declare/heavyload" />
-/// <reference types="shared/declare/typease" />
+/// <reference types="typease/index" />
 
 import type { Context, Next } from 'koa';
 declare type MiddleWareFn = (ctx: Context, next: Next) => void;
