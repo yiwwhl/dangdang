c39c1509bcdecbe2024a169d761c6efa0aabd96c
yiwwhl <yiwwhl@yiwwhl.local>
2023-03-09 17:57:53 +0800

    chore: add typease to web

--- packages/web/src/vite-env.d.ts
+++ packages/web/src/vite-env.d.ts
@@ -1,3 +1,4 @@
 /// <reference types="vite/client" />
 /// <reference types="shared/declare/expansion" />
 /// <reference types="shared/declare/heavyload" />
+/// <reference types="typease/index" />
